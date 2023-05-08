8a2f8749d30a5daf61c6b0fcb5085fbbb52d79d7
EwweKovacevic <kovacevic.erwin@gmail.com>
2023-05-08 15:11:13 +0200

    Update users.php

--- users.php
+++ users.php
@@ -5,6 +5,34 @@ $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 $query = "SELECT * FROM users";
 $result = mysqli_query($conn, $query);
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+  <title>User List</title>
+</head>
+<body>
+  <h1>User List</h1>
+  <table>
+    <thead>
+      <tr>
+        <th>Username</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php while ($row = mysqli_fetch_assoc($result)) { ?>
+        <tr>
+          <td><?php echo $row['username']; ?></td>
+          <td><?php echo $row['email']; ?></td>
+        </tr>
+      <?php } ?>
+    </tbody>
+  </table>
+</body>
+</html>
+
+<?php
 mysqli_close($conn);
 ?>
